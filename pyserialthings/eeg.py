4f4214a74693434938efc432e38dbbf7ba9b88aa
beepmo@student.ubc.ca <joannawjx@gmail.com>
2024-03-23 11:42:29 -0700

    waveforms displayed with massive delay

--- pyserialthings/eeg.py
+++ pyserialthings/eeg.py
@@ -6,11 +6,26 @@ import matplotlib.pyplot as plt
 com = "COM5"
 baud = 9600
 
-x = serial.Serial(com, baud, timeout = 0.05)
+signal = []
 
-start = time.time()
-
-while x.isOpen() == True:
-    data = x.read(1)
-    if len(data) > 0:
-        print(data)
+with serial.Serial(com, baud, timeout = 0.1) as x:
+    
+    # flush
+    x.reset_input_buffer()
+    x.reset_output_buffer()
+    x.flush()
+    
+    while x.isOpen() == True:
+        data = x.read(2)
+        if len(data) > 0:
+            print(data)
+            
+            # read integer ADC12MEM0 from bytes: little-endian, unsigned
+            ADC12 = int.from_bytes(data, "little")
+            print(ADC12)
+            volts = ADC12 / 4095 * 3.3
+            print(f'ADC12MEM0 is {ADC12}, which gives V = {volts}')
+            signal.append(volts)
+            
+            plt.plot(signal,'.')
+            plt.show()
--- roboarm/main.c
+++ roboarm/main.c
@@ -1,23 +1,19 @@
 #include <msp430.h> 
 
-void roboarm();
+static short x;
+void roboarm(void);
+void UARTConfigure(void);
 void UARTSendArray(char *TxArray, char ArrayLength);
+void Blink_Target_LED(void);
 
 int main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	/* Configure UART */
-	UCA1CTL1 = UCSWRST; //Recommended to place USCI in reset first
-	P4SEL |= BIT4 + BIT5;
-	UCA1CTL1 |= UCSSEL_2; // Use SMCLK
-	UCA1BR0 = 109; // Set baud rate to 9600 with 1.048MHz clock (Data Sheet 36.3.13)
-	UCA1BR1 = 0; // Set baud rate to 9600 with 1.048MHz clock
-	UCA1MCTL = UCBRS_2; // Modulation UCBRSx = 2
-	UCA1CTL1 &= ~UCSWRST; // Initialize USCI state machine
-	UCA1IE = UCRXIE;  // Enable  RX interrupt
 
-	UARTSendArray("Hello", 5);
+	UARTConfigure();
+	
+	int data[2]={1023, 235};
+	UARTSendArray(data, 4);
 
 	// set up ADC12 on P6.0
 	ADC12CTL0 = ADC12SHT02 + ADC12ON;
@@ -31,24 +27,39 @@ int main(void)
 
 	    while (ADC12CTL1 & ADC12BUSY); // poll while busy
 
-	    int x = ADC12MEM0;
+        // store ADC12MEM0 register (16 bits long, 4 bits empty) in 16-bit short
+	    x = ADC12MEM0;
+	    UARTSendArray(&x, 2);
+
 	}
 
 	return 0;
 }
 
+void UARTConfigure() {
+    /* Configure UART */
+    UCA1CTL1 = UCSWRST; //Recommended to place USCI in reset first
+    P4SEL |= BIT4 + BIT5;
+    UCA1CTL1 |= UCSSEL_2; // Use SMCLK
+    UCA1BR0 = 109; // Set baud rate to 9600 with 1.048MHz clock (Data Sheet 36.3.13)
+    UCA1BR1 = 0; // Set baud rate to 9600 with 1.048MHz clock
+    UCA1MCTL = UCBRS_2; // Modulation UCBRSx = 2
+    UCA1CTL1 &= ~UCSWRST; // Initialize USCI state machine
+    UCA1IE = UCRXIE;  // Enable  RX interrupt
+}
+
 void UARTSendArray(char *TxArray,  char ArrayLength){
- // Send number of bytes Specified in ArrayLength in the array at using the  UART
- // Example usage: UARTSendArray("Hello", 5);
- // int data[2]={1023, 235};
- // UARTSendArray(data, 4); // Note because the UART transmits bytes it is necessary to send two bytes for each integer hence the data length is twice the array length
+     // Send number of bytes Specified in ArrayLength in the array at using the  UART
+     // Example usage: UARTSendArray("Hello", 5);
+     // int data[2]={1023, 235};
+     // UARTSendArray(data, 4); // Note because the UART transmits bytes it is necessary to send two bytes for each integer hence the data length is twice the array length
 
-while(ArrayLength--){ // Loop until StringLength == 0 and post decrement
-while (! (UCA1IFG & UCTXIFG)); // wait for TX buffer to be ready for new data
+    while(ArrayLength--){ // Loop until StringLength == 0 and post decrement
+    while (! (UCA1IFG & UCTXIFG)); // wait for TX buffer to be ready for new data
 
- UCA1TXBUF = *TxArray; //Write the character at the location specified by the pointer
- TxArray++; //Increment the TxString pointer to point to the next character
- }
+     UCA1TXBUF = *TxArray; //Write the character at the location specified by the pointer
+     TxArray++; //Increment the TxString pointer to point to the next character
+     }
 }
 
 void roboarm() {
@@ -63,4 +74,14 @@ void roboarm() {
     TA0CTL = TASSEL_2 + MC_1 + TAIE + ID_0;
 
     _bis_SR_register(LPM0_bits + GIE);
+}
+
+// Function to Blink the LED
+void Blink_Target_LED(void)
+{
+    P1DIR |= BIT0;
+    P1OUT |= BIT0; // Turn ON LED on MSP430
+    __delay_cycles(200000); // wait for 0.2 sec
+    P1OUT &= ~BIT0; // Turn OFF LED on MSP430
+    __delay_cycles(200000); // wait for 0.2 sec
 }
