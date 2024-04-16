2bc0b6a646d76171595565278098c4d8df0650e5
beepmo@student.ubc.ca <joannawjx@gmail.com>
2024-04-15 18:29:53 -0700

    cleaned!

--- roboarm/main.c
+++ roboarm/main.c
@@ -1,16 +1,8 @@
 #include <msp430.h> 
 
 static short x; // store ADC12MEM0
-
 void UARTConfigure(void);
 void UARTSendArray(char *TxArray, char ArrayLength);
-void Blink_Target_LED(void);
-
-#define CENTER 1500 // 12 o'clock
-#define R1 1300 // 1 o'clock
-#define L1 1700 // 11 o'clock
-#define EPSILON 5 // increment
-#define DT 20000 // cycles per increment (0.02s)
 
 int main(void)
 {
@@ -31,11 +23,13 @@ int main(void)
     TA0CCR0 = 20000; // period: 20000 1MHz counts = 20ms
     TA0CCTL1 = OUTMOD_7; // periods start on high
     TA0CCR1 = 1500; // start servo on center
-    TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;  // begin output
+
+    // whir roboarm to center
+    TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;
     __delay_cycles(1000000);
     TA0CTL = 0;
 
-    _BIS_SR (LPM0_bits | GIE);
+    _BIS_SR (LPM0_bits | GIE); // CPU off, await interrupts
 
 	return 0;
 }
@@ -53,56 +47,40 @@ void UARTConfigure(void) {
 }
 
 /*
- * Interrupt service routine upon receiving a byte
+ * Interrupt service routine upon receiving a byte.
  * This ISR controls its own flag; no need to lower it manually.
- * However, don't let it call functions, or it won't return control properly to main.
+ * However, don't let it call functions or leave TimerA on, or it won't return control properly to main.
  */
 void __attribute__ ((interrupt(USCI_A1_VECTOR))) UCIV1_ISR(void) {
     char data = UCA1RXBUF;
 
     switch(data){
-        case 'd':
+        case 'd': // "give me a data point!"
+
             ADC12CTL0 |= ADC12SC; // start sampling
 
-
             x = ADC12MEM0;
+            // transmit ADC12 in 2 bytes
             char* ptr = &x;
-
             while (! (UCA1IFG & UCTXIFG)); // wait for TX buffer to be ready for new data
             UCA1TXBUF = *ptr;
-
             while (! (UCA1IFG & UCTXIFG)); // wait for TX buffer to be ready for new data
             UCA1TXBUF = *(ptr + 1);
         break;
 
-        case 'r':
-
+        case 'r': // "move right!"
             TA0CCR1 = TA0CCR1 + 10;
-            TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;  // begin output
+            TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;
             __delay_cycles(80000);
             TA0CTL = 0;
-
         break;
 
-        case 'l':
+        case 'l': // "move left!"
             TA0CCR1 = TA0CCR1 - 10;
-            TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;  // begin output
+            TA0CTL = TASSEL__SMCLK + MC_1 + TAIE + ID_0;
             __delay_cycles(80000);
             TA0CTL = 0;
 
         break;
     }
-}
-
-
-// Function to Blink the LED
-void Blink_Target_LED(void)
-{
-    P1DIR |= BIT0;
-    P1OUT |= BIT0; // Turn ON LED on MSP430
-    __delay_cycles(200000); // wait for 0.2 sec
-    P1OUT &= ~BIT0; // Turn OFF LED on MSP430
-    __delay_cycles(200000); // wait for 0.2 sec
-    P1OUT |= BIT0; // Turn ON LED on MSP430
-        __delay_cycles(200000); // wait for 0.2 sec
 }
